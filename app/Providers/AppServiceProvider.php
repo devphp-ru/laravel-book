89d78a42743491546abcb16a35ac62df01a5f5a2
devphpru <twebxak@gmail.com>
2023-10-15 18:52:11 +0300

    зарегистрировал зависимости book service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,20 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\SiteController;
+use App\Models\Book;
 use App\Models\Category;
+use App\Services\Books\BookServiceInterface;
+use App\Services\Books\Commands\CommandQueryBookHandler;
+use App\Services\Books\ReadBookService;
+use App\Services\Books\Repositories\Eloquent\ReadEloquentBookRepository;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class AppServiceProvider
+ *
+ * @package App\Providers
+ */
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,8 +35,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+    	$this->registerBindings();
+
         view()->composer('layouts.default', function ($view) {
 			$view->with('categories', Category::get());
 		});
     }
+
+	/**
+	 * Регистрация в контейнере зависимостей
+	 */
+    private function registerBindings(): void
+	{
+		$this->app->when(SiteController::class)
+			->needs(BookServiceInterface::class)
+			->give(function () {
+				return new ReadBookService(
+					new CommandQueryBookHandler(
+						new ReadEloquentBookRepository(
+							new Book()
+						)
+					)
+				);
+			});
+	}
+
 }
