6a5d61423650ccfaa3f822af3608df21fe12e15b
devphpru <twebxak@gmail.com>
2023-10-15 18:54:23 +0300

    подключил сервис книг в контроллере

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,20 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Book;
+use App\Services\Books\BookServiceInterface;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
+/**
+ * Class SiteController
+ *
+ * @package App\Http\Controllers
+ */
 class SiteController extends Controller
 {
+	/** @var BookServiceInterface  */
+	private BookServiceInterface $readBookService;
+
 	/**
+	 * SiteController constructor.
+	 *
+	 * @param BookServiceInterface $bookService
+	 */
+	public function __construct(BookServiceInterface $bookService)
+	{
+		$this->readBookService = $bookService;
+	}
+
+	/**
+	 * Показать главную страницу
+	 *
+	 * @param Request $request
 	 * @return View
 	 */
-    public function index(): View
+    public function index(Request $request): View
 	{
-		$books = Book::with('author')->orderByDesc('id')->paginate(10);
+		$books = $this->readBookService->getAllWithPaginate($request, 10);
 
 		return view('sites.index', [
 			'paginator' => $books,
 		]);
 	}
+
 }
